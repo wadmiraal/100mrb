a5a0e95d41a21cc4ca0ed1ee3df89cbe6b73e255
Wouter Admiraal <wad@wadmiraal.net>
2014-02-10 21:22:12 +0100

    Update sync() test.

--- front-end/test/spec/js/collection/ratings.js
+++ front-end/test/spec/js/collection/ratings.js
@@ -20,10 +20,53 @@ define([ 'app/collection/ratings' ], function( RatingCollection ) {
         describe( 'when instantiated', function() {
             it( 'should have a different URL scheme if linked to a book', function() {
                 var ratings = new RatingCollection();
-                expect( ratings.url() ).toEqual( '/ratings' );
+                expect( _.result( ratings, 'url' ) ).toEqual( '/ratings' );
 
-                var ratings = new RatingCollection( [], { bookId: 1 } );
-                expect( ratings.url() ).toEqual( '/book/1/ratings' );
+                var bookRatings = new RatingCollection( [], { bookId: 1 } );
+                expect( _.result( bookRatings, 'url' ) ).toEqual( '/book/1/ratings' );
+            });
+
+            it( 'should make a correct request when synced', function() {
+                var ratings = new RatingCollection();
+                var server = sinon.fakeServer.create();
+                server.respondWith(
+                    'GET',
+                    '/ratings',
+                    [
+                        200,
+                        { 'Content-Type': 'application/json' },
+                        '{ "response": "[ { rating: 1, userId: 1, bookId: 1 }, { rating: 2, userId: 2, bookId: 2 } ]" }'
+                    ]
+                );
+                server.respondWith(
+                    'GET',
+                    '/book/1/ratings',
+                    [
+                        200,
+                        { 'Content-Type': 'application/json' },
+                        '{ "response": "[ { rating: 1, userId: 1, bookId: 1 }, { rating: 2, userId: 2, bookId: 2 } ]" }'
+                    ]
+                );
+
+                ratings.fetch();
+                server.respond();
+
+                expect( server.requests.length ).toEqual( 1 );
+                var request = server.requests[0];
+                expect( request.method ).toEqual( 'GET' );
+                expect( request.url ).toEqual( '/ratings' );
+
+                ratings = new RatingCollection( [], { bookId: 1 } );
+
+                ratings.fetch();
+                server.respond();
+
+                expect( server.requests.length ).toEqual( 2 );
+                var request = server.requests[1];
+                expect( request.method ).toEqual( 'GET' );
+                expect( request.url ).toEqual( '/book/1/ratings' );
+
+                server.restore();
             });
         });
     });
