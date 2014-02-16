300096c026d0a50f99ab4ee37bd4a6c6eb8cbd89
Wouter Admiraal <wad@wadmiraal.net>
2014-02-16 12:40:00 +0100

    Add dev html files. Add Compass integration.

---
+++ front-end/config.rb
@@ -0,0 +1,16 @@
+##
+## This file is only needed for Compass/Sass integration. If you are not using
+## Compass, you may safely ignore this file.
+##
+
+css_dir = "app/assets/css"
+sass_dir = "app/assets/sass"
+images_dir = "app/assets/img"
+generated_images_dir = images_dir + "/generated"
+javascripts_dir = "app/assets/js"
+
+require 'compass-normalize'
+
+output_style = :compressed
+relative_assets = true
+line_comments = false
